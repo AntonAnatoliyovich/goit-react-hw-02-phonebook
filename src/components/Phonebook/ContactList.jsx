307a1487672908876df830dcedc10dc450e36694
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-03-28 17:58:11 +0300

    finish

--- src/components/Phonebook/ContactList.jsx
+++ src/components/Phonebook/ContactList.jsx
@@ -1,25 +1,38 @@
+import PropTypes from 'prop-types';
 import css from './Phonebook.module.css';
 
-export const ContactList = ( { contacts, filteredContacts, deleteContact } ) => {
+export const ContactList = ({ contacts, children, deleteContact }) => {
     return (
-        <>
-            <ul className={css.contact__List}>
-                {filteredContacts.length > 0 ?
-                    filteredContacts.map((contact) => (
-                        <>
-                        <li key={contact.id} className={css.contact__item}>{contact.name}: {contact.number}</li>
-                        <button onClick={()=>{deleteContact(contact.id, 'filter')}} className={css.contact__button} type='submit'>Delete</button>
-                        </>
-                    ))
-                :
-                    contacts.map((contact) => (
-                        <>
-                        <li key={contact.id} className={css.contact__item}>{contact.name}: {contact.number}</li>
-                        <button onClick={()=>{deleteContact(contact.id, 'contacts')}} className={css.contact__button} type='submit'>Delete</button>
-                        </>
-                    ))
-                }
-            </ul>
-        </>
-    )
-}
+        <div className={css.contactlist__card}>
+            <h2 className={css.phonebook__title}>Contacts</h2>
+                {children}
+                <ul className={css.contact__List}>
+                    {contacts.map(({ id, name, number }) => (
+                    <li className={css.contact__item} key={id}>
+                        <p className={css.contact__text}>{name}</p>
+                        <p className={css.contact__text}> {number}</p>
+                        <button
+                        className={css.contact__button}
+                        onClick={() => {
+                        deleteContact(id);
+                        }}
+                        >
+                        Delete
+                        </button>
+                    </li>
+                    ))}
+                </ul>
+        </div>
+    );
+};
+
+ContactList.propTypes = {
+    contacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+        })
+    ),
+    deleteContact: PropTypes.func,
+};
--- src/components/Phonebook/Filter.jsx
+++ src/components/Phonebook/Filter.jsx
@@ -1,10 +1,28 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import css from './Phonebook.module.css';
 
-export const Filter = ( {onFilter} ) => {
-    return (
-        <div className={css.filter}>
-            <label className={css.filter__label} htmlFor="">Find contacts by name</label>
-            <input onChange={(e)=>{onFilter(e.target.value)}} className={css.filter__input} type="text" />
-        </div>
-    )
+
+export class Filter extends Component {
+    render() {
+        const { filter, addFilter } = this.props;
+        return (
+            <div className={css.filter}>
+                <label className={css.filter__label} htmlFor="">Find contacts by name</label>
+                <input
+                    className={css.filter__input}
+                    type="text"
+                    name="filter"
+                    value={filter}
+                    onChange={addFilter}
+                    placeholder="Enter name"
+                />
+            </div>
+        );
+    }
 }
+
+Filter.propTypes = {
+    filter: PropTypes.string.isRequired,
+    addFilter: PropTypes.func,
+};
